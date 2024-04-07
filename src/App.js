d117b7d3e43c8933b78a5f0e3fcbf367fb3daeb0
boburastonov <boburastonov33@gmail.com>
2024-04-07 08:38:41 +0500

    Add material-ui icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import './App.css';
+import "./App.css";
+import Btn from "@mui/material/Button";
+import SaveIcon from "@mui/icons-material/Save";
+import CheckBox from "@mui/icons-material/CheckBox";
+import RemoveIcon from "@mui/icons-material/RemoveCircleOutline";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Btn
+        variant="contained"
+        color="success"
+        endIcon={<SaveIcon style={{ color: "lightcyan" }} />}
+        size="medium"
+        style={{ color: "red" }}
+      >
+        Save me!
+      </Btn>
+      <CheckBox />
+      <Btn
+        variant="contained"
+        endIcon={<RemoveIcon style={{ color: "orange" }} />}
+        style={{ textDecoration: "line-through", marginLeft: "10px" }}
+        disabled
+      >
+        Removed!
+      </Btn>
     </div>
   );
 }
